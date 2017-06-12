b2a8c847598a58eac20b7c73465f4049809bddfe
pascas123 <pasqual.kossek@googlemail.com>
2017-06-12 13:20:23 +0200

    structsForBreak.c

---
+++ structsForBreak.c
@@ -0,0 +1,18 @@
+#define MAXCYCLESBEFORE 1000
+#define MAXCYCLESAFTER 10000
+
+// Speed and distance before using the break
+struct speedAndDisctanceBefore{
+int speedBefore;
+int distanceBefore;	
+};
+struct speedAndDisctanceBefore incomingDateBefore[MAXCYCLESBEFORE];
+
+
+//Speed and Distance after using the break
+
+struct struct speedAndDisctanceAfter{
+int speedAfter;
+int distanceAfter;	
+};
+struct speedAndDisctanceAfter incomingDateAfter[MAXCYCLESAFTER];
